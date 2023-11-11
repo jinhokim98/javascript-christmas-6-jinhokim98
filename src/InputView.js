c4aa35b8364fdc46203dc3b61cf4cb41f158c955
김진호 <rlawlsgh1227@gmail.com>
2023-11-11 11:34:24 +0900

    fix: InputView, OutputView 메일 공지대로 변경

--- src/InputView.js
+++ src/InputView.js
@@ -1,6 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 
-export default InputView = {
+const InputView = {
   async readDate() {
     const input = await Console.readLineAsync(
       '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
@@ -9,3 +9,5 @@ export default InputView = {
   },
   // ...
 };
+
+export default InputView;
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,9 +1,11 @@
 import { Console } from '@woowacourse/mission-utils';
 
-export default OutputView = {
+const OutputView = {
   printMenu() {
     Console.print('<주문 메뉴>');
     // ...
   },
   // ...
 };
+
+export default OutputView;
