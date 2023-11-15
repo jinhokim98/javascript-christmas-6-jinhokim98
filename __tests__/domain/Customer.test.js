654d5daaa853901925098cba58c2cfd0dfd92647
김진호 <rlawlsgh1227@gmail.com>
2023-11-15 16:59:23 +0900

    test: 총 혜택금액 검증

--- __tests__/domain/Customer.test.js
+++ __tests__/domain/Customer.test.js
@@ -1,5 +1,6 @@
 import Customer from "../../src/domain/Customer";
 import Order from "../../src/domain/Order";
+import VisitDay from "../../src/domain/VisitDay";
 
 describe('고객 테스트', () => {
   describe('할인 테스트', () => {
@@ -15,6 +16,21 @@ describe('고객 테스트', () => {
       customer.giveDuplicateDiscount();
 
       // then
+      expect(customer.getTotalBenefitsAmount()).toEqual(ANSWER);
+    });
+
+    const INPUTS = [['12', '해산물파스타-2,아이스크림-1', 4123], ['25', '바비큐립-1,레드와인-1,초코케이크-2', 33446], ['30', '바비큐립-2', 4046]];
+    test.each(INPUTS)('총 혜택 금액 검증하기', (day, order, answer) => {
+      // given
+      const VISIT_DAY = new VisitDay(day);
+      const ORDER_MENU = order;
+      const ANSWER = answer;
+
+      // when
+      const customer = new Customer(VISIT_DAY);
+      customer.completeOrder(new Order(ORDER_MENU));
+      customer.giveDuplicateDiscount();
+
       expect(customer.getTotalBenefitsAmount()).toEqual(ANSWER);
     });
   });
