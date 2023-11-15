3b74efd4ec60491bac573302e9824446976acd09
김진호 <rlawlsgh1227@gmail.com>
2023-11-15 16:06:54 +0900

    test: 특별 할인 테스트

--- __tests__/domain/Event.test.js
+++ __tests__/domain/Event.test.js
@@ -1,7 +1,9 @@
 import Order from "../../src/domain/Order.js";
 import VisitDay from "../../src/domain/VisitDay";
 import ChristmasDDay from "../../src/domain/event/ChristmasDDay.js";
+import Specific from "../../src/domain/event/Specific.js";
 import Weekday from "../../src/domain/event/Weekday.js";
+import Weekend from "../../src/domain/event/Weekend.js";
 
 describe('이벤트 테스트', () => {
   describe('크리스마스 디데이 할인 테스트', () => {
@@ -78,5 +80,78 @@ describe('이벤트 테스트', () => {
       expect(applied).toEqual(ANSWER);
     });
   });
-  
+  describe('주말 할인 테스트', () => {
+    test('주중에 방문할 경우 주말 할인은 없다.', () => {
+      // given
+      const VISIT_DAY = new VisitDay(27);
+      const ORDER = new Order('바비큐립-1,제로콜라-2');
+      const ANSWER = 0;
+
+      // when
+      const applied = Weekend.apply(VISIT_DAY, ORDER)
+
+      // then
+      expect(applied).toEqual(ANSWER);
+    });
+    test('주말에 방문했지만 주문 중에 메인메뉴가 없으면 주말 할인은 없다.', () => {
+      // given
+      const VISIT_DAY = new VisitDay(23);
+      const ORDER = new Order('타파스-1,제로콜라-2');
+      const ANSWER = 0;
+
+      // when
+      const applied = Weekend.apply(VISIT_DAY, ORDER)
+
+      // then
+      expect(applied).toEqual(ANSWER);
+    });
+    test('주말에 방문했고 바비큐립을 1개 주문하면 주말 할인은 2023원이다.', () => {
+      // given
+      const VISIT_DAY = new VisitDay(23);
+      const ORDER = new Order('바비큐립-1,제로콜라-2');
+      const ANSWER = 2023;
+
+      // when
+      const applied = Weekend.apply(VISIT_DAY, ORDER)
+
+      // then
+      expect(applied).toEqual(ANSWER);
+    });
+    test('주말에 방문했고 바비큐립을 2개 주문하고 크리스마스파스타를 1개 주문하면 주말 할인은 6069원이다.', () => {
+      // given
+      const VISIT_DAY = new VisitDay(23);
+      const ORDER = new Order('바비큐립-2,크리스마스파스타-1');
+      const ANSWER = 6069;
+
+      // when
+      const applied = Weekend.apply(VISIT_DAY, ORDER)
+
+      // then
+      expect(applied).toEqual(ANSWER);
+    });
+  });
+  describe('특별 할인 테스트', () => {
+    test('특별한 날짜에 방문하지 않은 경우 특별 할인은 없다.', () => {
+      // given
+      const VISIT_DAY = new VisitDay(4);
+      const ANSWER = 0;
+
+      // when
+      const applied = Specific.apply(VISIT_DAY)
+
+      // then
+      expect(applied).toEqual(ANSWER);
+    });
+    test('특별한 날짜에 방문한 경우 특별 할인은 1000원이다.', () => {
+      // given
+      const VISIT_DAY = new VisitDay(25);
+      const ANSWER = 1000;
+
+      // when
+      const applied = Specific.apply(VISIT_DAY)
+
+      // then
+      expect(applied).toEqual(ANSWER);
+    });
+  });
 });
