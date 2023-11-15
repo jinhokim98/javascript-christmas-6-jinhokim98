11ff6e727b177c02b35e7e714883bcc62c823142
김진호 <rlawlsgh1227@gmail.com>
2023-11-15 21:34:39 +0900

    test: 이벤트 통계 기능 구현 (이벤트 목표 달성 체크를 위해)
    12월 총 판매 금액, 고객 별 배지발급 현황

--- src/EventStatistics.js
+++ src/EventStatistics.js
@@ -8,6 +8,29 @@ class EventStatistics {
   enrollCustomer(customer) {
     this.#customersInfo.push(customer);
   }
+
+  // 12월 총 판매 금액 (할인 후 금액) 통계
+  calculateSalesPerformanceThisEvent() {
+    if (this.#customersInfo.length <= 0) return null;
+
+    return this.#customersInfo.reduce(
+      (acc, customer) => acc + customer.getTotalBillAfterDiscount(),
+      0,
+    );
+  }
+
+  // 고객 별 배지발급 현황 (새해 이벤트 때 활용하기 위해)
+  putTogetherTotalIssuedBadge() {
+    if (this.#customersInfo.length <= 0) return null;
+
+    const totalIssuedBadge = [];
+
+    this.#customersInfo.forEach((customer) => {
+      totalIssuedBadge.push({ customer, badge: customer.loadEventBadge() });
+    });
+
+    return totalIssuedBadge;
+  }
 }
 
 export default EventStatistics;
